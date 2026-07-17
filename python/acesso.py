9813f33404a7f59805bc7fbc9051b76b3a4a4ea0
euodeene <danielfinotti2@gmail.com>
2026-07-17 19:06:58 -0300

    atualizando com comentarios nos codigos...

--- python/acesso.py
+++ python/acesso.py
@@ -1,6 +1,16 @@
+# idade = variavel
+# possui_convite = variavel verdadeira 
+
 idade = input("Digite Sua Idade:")
 possui_convite = True
+
+# if = se 
+# idade = variavel 
+# >= = maior igual
+# 18 and possui_convite == True = 18 for maior e se possui convite dar 
+# Verdadeiro: Entrada Autorizada. Falso: Entrada negada.
+
 if idade >= 18 and possui_convite == True:
     print("Entrada Autorizada.")
 else:
-    print("entrada Negada.")
+    print("Entrada Negada.")
--- python/desafio.py
+++ python/desafio.py
@@ -1,7 +1,21 @@
+# VARIAVEIS: dados_nomes e pesquisa 
+# dados_nomes = ["Ana", "Bruno", "Carlos", "Daniela", "Eduardo"]
+# pesquisa = input("Digite Seu usuario.: ")
+
 dados_nomes = ["Ana", "Bruno", "Carlos", "Daniela", "Eduardo"]
 pesquisa = input("Digite Seu usuario.: ")
 
-if pesquisa in dados_nomes:
-    print("Aluno Matriculado")
+
+if pesquisa in dados_nomes:     
+    print("Aluno Matriculado")  
 else: 
     print("Aluno Não encontrado.")
+
+
+
+# if pesquisa in dados_nomes: 
+# pesquisa = variavel
+# dados_nomes = variavel
+# if = se
+# in = verifica se a algo dentro da lista, string, e etc...
+# else = ao contrario de if 
