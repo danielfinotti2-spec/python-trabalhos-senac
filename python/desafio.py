a91fce05f0c3e0d75bfd8d984e976d6674cc0552
euodeene <danielfinotti2@gmail.com>
2026-07-17 18:47:56 -0300

    desafio

---
+++ python/desafio.py
@@ -0,0 +1,7 @@
+dados_nomes = ["Ana", "Bruno", "Carlos", "Daniela", "Eduardo"]
+pesquisa = input("Digite Seu usuario.: ")
+
+if pesquisa in dados_nomes:
+    print("Aluno Matriculado")
+else: 
+    print("Aluno Não encontrado.")
